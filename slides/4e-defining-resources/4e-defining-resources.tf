491a3a8c06a2808f44cb2063833306ff087562ce
Kevin Yeh <kevinyeah@utexas.edu>
2018-11-14 16:52:02 -0500

    Use data soruce

--- slides/4e-defining-resources/4e-defining-resources.tf
+++ slides/4e-defining-resources/4e-defining-resources.tf
@@ -3,34 +3,28 @@
                           # # # # # # # # # # # ##
 
 data "aws_vpc" "workshop_vpc" {
-  cidr_block = "10.0.241.0/24"
-
   tags {
     source = "QPP Knowledge Share"
   }
 }
 
 data "aws_subnet" "public_subnet_1" {
-  vpc_id = "${aws_vpc.workshop_vpc.id}"
-  availability_zone = "us-east-1a"
-  cidr_block = "10.0.241.0/26"
-
-  tags {
-    source = "QPP Knowledge Share"
-  }
+  vpc_id = "${data.aws_vpc.workshop_vpc.id}"
 }
 
-# ami-c481fad3
 # variable "ami_id" {
 #     description = "Launch with this AMI id."
 #     type        = "string"
-#     default     = "ami-123"
+#     default     = "ami-6871a115"
 # }
-#
+
 # resource "aws_instance" "bastion" {
 #     ami = "${var.ami_id}"
-#     subnet_id = "${aws_subnet.public_subnet_1.id}"
+#     subnet_id = "${data.aws_subnet.public_subnet_1.id}"
 #     associate_public_ip_address = true
-#     key_name = "[key name]"
+#     key_name = "kevin-tf-knowledge-share-test"
 #     instance_type = "t2.micro"
 # }
+
+# note for kevin don't look
+# alt: ami-e3063199
